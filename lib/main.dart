ad6906e0aad56fd0f10acf70e96f6b6ff43138ec
KisoththamanM <mkisoththamanofficial@gmail.com>
2025-12-14 11:23:40 +0530

    Firebase integration for web app has been initialized in the main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:login_with_firebase/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "*****",
+      appId: "*****",
+      messagingSenderId: "*****",
+      projectId: "login-with-firebase-*****",
+    ),
+  );
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Login();
